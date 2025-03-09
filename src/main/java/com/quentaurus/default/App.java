0ce5774b9b14bcdd8b3ed9f3de1296f51b4c6b77
Anders Averö <quentaurus@gmail.com>
2025-03-09 17:25:33 +0100

    INitial commit

---
+++ src/main/java/com/quentaurus/default/App.java
@@ -0,0 +1,17 @@
+package com.quentaurus;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println(token());
+    }
+	
+	private static String token() {
+		return "Hello World!";
+	}
+}
