9084f15c6794adbf9ab8835c811a3119d568452a
Anders Averö <quentaurus@gmail.com>
2025-03-11 06:16:35 +0100

    More updates

--- src/main/java/com/quentaurus/defaults/App.java
+++ src/main/java/com/quentaurus/defaults/App.java
@@ -1,17 +1,15 @@
-package com.quentaurus;
+package com.quentaurus.defaults;
 
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println(token());
-    }
-	
-	private static String token() {
+public class App {
+	public static void main(String[] args) {
+		System.out.println(token());
+	}
+
+	static String token() {
 		return "Hello World!";
 	}
 }
